bc85b23312b473e550fba5b56cce8cf07d3763a7
KampusHub <verbelesteban2@gmail.com>
2017-07-27 22:03:14 -0400

    Added MainViewModel and set BindingContext

--- NextHolliday/NextHolliday/NextHolliday/MainPage.xaml.cs
+++ NextHolliday/NextHolliday/NextHolliday/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NextHolliday.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,10 @@ namespace NextHolliday
         public MainPage()
         {
             InitializeComponent();
+
+            MainViewModel viewModel = new MainViewModel();
+
+            BindingContext = viewModel;
         }
     }
 }
---
+++ NextHolliday/NextHolliday/NextHolliday/ViewModels/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using MvvmHelpers;
+using NextHolliday.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextHolliday.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+
+        #region -- Properties --
+
+        private RemainingTime _remainingTime;
+
+        public RemainingTime RemainingTime
+        {
+            get { return _remainingTime; }
+            set
+            {
+                _remainingTime = value;
+                SetProperty(ref _remainingTime, value);
+            }
+        }
+
+        #endregion
+
+        #region -- Constructor --
+
+        public MainViewModel()
+        {
+
+        }
+
+        #endregion
+
+    }
+}
