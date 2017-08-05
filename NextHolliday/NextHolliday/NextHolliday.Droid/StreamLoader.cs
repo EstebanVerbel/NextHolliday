e363f8d54943cefda2d8ab4dbd515d1da66471ea
KampusHub <verbelesteban2@gmail.com>
2017-08-05 15:29:18 -0400

    Added stream loader to Android project

---
+++ NextHolliday/NextHolliday/NextHolliday.Droid/StreamLoader.cs
@@ -0,0 +1,22 @@
+﻿using Android.Content;
+using NextHolliday.Models.Repository;
+using System.IO;
+
+namespace NextHolliday.Droid
+{
+    public class StreamLoader : IStreamLoader
+    {
+        private readonly Context _context;
+
+        public StreamLoader(Context context)
+        {
+            // Android assets are loaded through an Android Context so pass in a Context to the constructor
+            _context = context;
+        }
+
+        public Stream GetStreamForFilename(string fileName)
+        {
+            return _context.Assets.Open(fileName);
+        }
+    }
+}
