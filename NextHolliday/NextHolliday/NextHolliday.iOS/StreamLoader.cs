1bb3ed7941690e501f9da759f0355139a6b9a41b
KampusHub <verbelesteban2@gmail.com>
2017-08-07 21:44:31 -0400

    Added Stream Loader class to iOS project

---
+++ NextHolliday/NextHolliday/NextHolliday.iOS/StreamLoader.cs
@@ -0,0 +1,13 @@
+﻿using NextHolliday.Models.Repository;
+using System.IO;
+
+namespace NextHolliday.iOS
+{
+    public class StreamLoader : IStreamLoader
+    {
+        public Stream GetStreamForFilename(string fileName)
+        {
+            return File.OpenRead(fileName);
+        }
+    }
+}
