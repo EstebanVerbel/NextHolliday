70cc738e69f2d5f654fc9c5cab587b64f77a0b21
KampusHub <verbelesteban2@gmail.com>
2017-10-26 20:04:16 -0400

    Implemented Settings class
    this class will be used to set users' settings on local file

--- NextHolliday/NextHolliday/NextHolliday/Models/Settings.cs
+++ NextHolliday/NextHolliday/NextHolliday/Models/Settings.cs
@@ -1,25 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Plugin.Settings;
 
 namespace NextHolliday.Models
 {
     public static class Settings
     {
-        private const string Country = "country";
-        private const string Province = "province";
+        #region -- Keys --
 
-        //public static string Country
-        //{
-        //    get
-        //    {
+        private const string CountryKey = "country";
+        private const string ProvinceKey = "province";
 
-        //    }
-        //}
+        #endregion
 
+        #region -- Properties --
 
+        public static string Country
+        {
+            get
+            {
+                return CrossSettings.Current.GetValueOrDefault(CountryKey, string.Empty);
+            }
+            set
+            {
+                CrossSettings.Current.AddOrUpdateValue(CountryKey, value);
+            }
+        }
+
+        public static string Province
+        {
+            get
+            {
+                return CrossSettings.Current.GetValueOrDefault(ProvinceKey, string.Empty);
+            }
+            set
+            {
+                CrossSettings.Current.AddOrUpdateValue(ProvinceKey, value);
+            }
+        }
+
+        #endregion
 
     }
 }
